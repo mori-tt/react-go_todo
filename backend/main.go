2cc1e6ec90149c22a3fa9b698ba2e9c599b6d0a1
T_Mori <morit0301@gmail.com>
2024-11-24 14:53:12 +0900

    Task API construction
    Constructions of Validator

--- backend/main.go
+++ backend/main.go
@@ -6,13 +6,19 @@ import (
 	"backend/repository"
 	"backend/router"
 	"backend/usecase"
+	"backend/validator"
 )
 
 func main() {
 	db := db.NewDB()
+	userValidator := validator.NewUserValidator()
+	taskValidator := validator.NewTaskValidator()
 	userRepository := repository.NewUserRepository(db)
-	userUsecase := usecase.NewUserUsecase(userRepository)
+	taskRepository := repository.NewTaskRepository(db)
+	userUsecase := usecase.NewUserUsecase(userRepository, userValidator)
+	taskUsecase := usecase.NewTaskUsecase(taskRepository, taskValidator)
 	userController := controller.NewUserController(userUsecase)
-	e := router.NewRouter(userController)
+	taskController := controller.NewTaskController(taskUsecase)
+	e := router.NewRouter(userController, taskController)
 	e.Logger.Fatal(e.Start(":8080"))
 }
